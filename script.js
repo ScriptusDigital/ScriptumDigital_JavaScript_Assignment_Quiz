70d69b0edf26167cd1dc55e8827733905c5905b3
ScriptusDigital <Scriptumdigital.ie@gmail.com>
2026-01-30 14:02:06 +0000

    Further fixes on JS

--- script.js
+++ script.js
@@ -45,13 +45,12 @@ function updateProgressBar() {
 function updateAnsweredCount() {
     let answered = 0;
     for (let i = 1; i <= TOTAL; i++) {
-        const name = "q" +i;
-        if (document.querySelector(`input[name="${name}"]:checked`)) {
+        if (document.querySelector(`input[name="q${i}"]:checked`)) {
             answered++;
         }
     }
-    answeredCount.textContent = answered;
 
+    answeredCount.textContent = answered;
     updateProgressBar();
 }
 
@@ -76,6 +75,44 @@ function calculateScore() {
     return score;
 }
 
+//Show when incorrect or correct//
+function showCorrectAnswers() {
+    const fieldsets = quizForm.querySelectorAll("fieldset");
+
+    for (let i = 1; i <= TOTAL; i++) {
+    const fs = fieldsets[i - 1];
+    const correctValue = answers[`q${i}`];
+    const chosen = fs.querySelector(`input[name="q${i}"]:checked`);
+
+//reset old fieldset and labels//
+fs.classList.remove("correct", "incorrect");
+fs.querySelectorAll("label").forEach(label => {
+    label.classList.remove("correctChoice", "wrongChoice");
+});
+
+//Values for when correct/incorrect//
+    if (chosen && chosen.value === correctValue) {
+        fs.classList.add("correct");}
+       else {
+        fs.classList.add("incorrect");
+       }
+    
+//Label correct and incorrect answers//
+fs.querySelectorAll("label").forEach(label => {
+    const input = label.querySelector('input[type="radio"]');
+    if (!input) return;
+
+    if (input.value === correctValue) {
+        label.classList.add("correctChoice");
+    }
+    if (chosen && chosen.value !== correctValue && input === chosen) {
+        label.classList.add("wrongChoice");
+    }
+});
+}
+}
+
+
 //Listener and count//
 quizForm.addEventListener("change", updateAnsweredCount);
 
@@ -93,8 +130,8 @@ quizForm.addEventListener("submit", function(event) {
     const score = calculateScore();
     showMessage("Thank you for completing the quiz!");
     result.textContent = `Your score is ${score} out of ${TOTAL}.`;
+    showCorrectAnswers();
 });
-
 
 //Form reset//
 resetBtn.addEventListener("click", function() {
@@ -102,9 +139,18 @@ resetBtn.addEventListener("click", function() {
     showMessage("");
     result.textContent = "";
     updateAnsweredCount();
-}); 
 
-//Initial setup//
+    document.querySelectorAll("fieldset").forEach(fs => {
+        fs.classList.remove("correct", "incorrect");
+
+document.querySelectorAll("label").forEach(label => {
+            label.classList.remove("correctChoice", "wrongChoice");
+        });
+
+        updateAnsweredCount();
+    });
+});
+
+//Initial count update//
 updateAnsweredCount();  
-showMessage("");
-
+showMessage("");    
